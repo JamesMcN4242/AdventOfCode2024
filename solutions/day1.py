67d43169bdef4e35a7e807c805ab0404a9dac8e5
James McNeil <jamesmcneil42@hotmail.co.uk>
2024-12-01 11:37:35 +0100

    Day one solution and inputs.

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,1 +1,19 @@
-print("day one file")
+lines = open("input/day1.txt").readlines()
+
+left = []
+right = []
+for x in lines:
+    components = x.split()
+    left.append(int(components[0]))
+    right.append(int(components[1]))
+
+left.sort()
+right.sort()
+partOne = 0
+partTwo = 0
+for i in range(len(left)):
+    partOne += abs(right[i] - left[i])
+    partTwo += left[i] * right.count(left[i])
+
+print("Part One:", partOne)
+print("Part Two:", partTwo)
